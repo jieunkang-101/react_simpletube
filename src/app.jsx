605154d718b815e808d736c472c6e53bb4cdcbd1
Jieun Kang <jieunkang101@gmail.com>
2021-04-26 12:11:10 -0700

    add service/youtube-fetch class

--- src/app.jsx
+++ src/app.jsx
@@ -1,24 +1,12 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import VideoList from './components/VideoList';
 
-const App = () => {
+const App = ({ youtube }) => {
   const [videos, setVideos] = useState([]);
-  console.log('S', videos);
 
   useEffect(() => {
-    var requestOptions = {
-      method: 'GET',
-      redirect: 'follow',
-    };
-
-    fetch(
-      'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&masResults=25&key=AIzaSyCUcqIsbiGkf3RcZThApa63j8wr9q4lquc',
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log('error', error));
-  }, []);
+    youtube.mostPopular().then((videos) => setVideos(videos));
+  }, [youtube]);
 
   return <VideoList videos={videos} />;
 };
