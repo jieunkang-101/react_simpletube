f82e9a81d099bcb1bb4251180a29527653fef25c
Jieun Kang <jieunkang101@gmail.com>
2021-04-26 12:37:16 -0700

    add SearchHeader component

--- src/app.jsx
+++ src/app.jsx
@@ -1,14 +1,26 @@
 import React, { useState, useEffect } from 'react';
 import VideoList from './components/VideoList';
+import SearchHeader from './components/SearchHeader';
+import styles from './App.module.css';
 
 const App = ({ youtube }) => {
   const [videos, setVideos] = useState([]);
+  const search = (query) => {
+    youtube
+      .search(query) //
+      .then((videos) => setVideos(videos));
+  };
 
   useEffect(() => {
     youtube.mostPopular().then((videos) => setVideos(videos));
   }, [youtube]);
 
-  return <VideoList videos={videos} />;
+  return (
+    <div className={styles.app}>
+      <SearchHeader onSearch={search} />
+      <VideoList videos={videos} />;
+    </div>
+  );
 };
 
 export default App;
--- src/components/VideoList/index.jsx
+++ src/components/VideoList/index.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import VideoItem from '../VideoItem';
+import styles from './index.module.css';
 
 const VideoList = (props) => (
-  <ul>
+  <ul className={styles.videos}>
     {props.videos.map((video) => (
       <VideoItem key={video.id} video={video} />
     ))}
