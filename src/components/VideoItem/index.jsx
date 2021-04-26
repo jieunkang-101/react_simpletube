74d78fa6f64eef4eee6ce4a7c21d53064980d1e2
Jieun Kang <jieunkang101@gmail.com>
2021-04-26 12:22:44 -0700

    add style module for VideoItem

--- src/components/VideoItem/index.jsx
+++ src/components/VideoItem/index.jsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import styles from './index.module.css';
 
-const VideoItem = (props) => <h1>{props.video.snippet.title}</h1>;
+const VideoItem = ({ video: { snippet: videoItem } }) => (
+  <li className={styles.video}>
+    <img
+      className={styles.thumbnail}
+      src={videoItem.thumbnails.medium.url}
+      alt="video thumnail"
+    />
+    <div>
+      <p className={styles.title}>{videoItem.title}</p>
+      <p className={styles.channel}>{videoItem.channelTitle}</p>
+    </div>
+  </li>
+);
 
 export default VideoItem;
