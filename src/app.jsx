d1cba1583db9544975051c97b39d3fb37afd8e16
Jieun Kang <jieunkang101@gmail.com>
2021-04-26 11:40:46 -0700

    add VideoList and VideoItem components

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,26 @@
-import './app.css';
+import React, { useEffect, useState } from 'react';
+import VideoList from './components/VideoList';
 
-function App() {
-  return (
-   <h1>Hello :)</h1>
-  );
-}
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  console.log('S', videos);
+
+  useEffect(() => {
+    var requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    };
+
+    fetch(
+      'https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&masResults=25&key=AIzaSyCUcqIsbiGkf3RcZThApa63j8wr9q4lquc',
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log('error', error));
+  }, []);
+
+  return <VideoList videos={videos} />;
+};
 
 export default App;
